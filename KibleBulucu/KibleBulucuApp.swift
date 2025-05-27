cf0fe7e19f9fc961a2ad3e1db179acee9ea2f4a9
Yunus Elçi <yunus.elci@hititcs.com>
2025-05-27 13:34:40 +0300

    Initial Commit

---
+++ KibleBulucu/KibleBulucuApp.swift
@@ -0,0 +1,17 @@
+//
+//  KibleBulucuApp.swift
+//  KibleBulucu
+//
+//  Created by Yunus Elçi on 27.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct KibleBulucuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
